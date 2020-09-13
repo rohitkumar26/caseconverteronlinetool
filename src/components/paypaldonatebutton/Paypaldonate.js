e8c0ea56f0ed21a534485de4c5ec0385e1739e21
Coder Rohit <rkumar26384@gmail.com>
2020-09-13 11:51:29 +0530

    paypal button added

--- src/components/paypaldonatebutton/Paypaldonate.js
+++ src/components/paypaldonatebutton/Paypaldonate.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Paypaldonate() {
     return (
         <div>
-            <h1>Support Us</h1>
+
             <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
                 <input type="hidden" name="cmd" value="_s-xclick" />
                 <input type="hidden" name="hosted_button_id" value="SAN7KZGDJR9BG" />
